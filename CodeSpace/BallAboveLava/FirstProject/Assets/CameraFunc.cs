a00c83a27163a465f2436ac1091aeeb4e685bfb0
Kaiyang Jiang <kaiyangj@usc.edu>
2023-08-29 21:47:21 -0700

    added projects

---
+++ CodeSpace/BallAboveLava/FirstProject/Assets/CameraFunc.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFunc : MonoBehaviour
+{
+    public Vector3 myPos;
+    public Transform myPlay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = myPlay.position + myPos;
+    }
+}
